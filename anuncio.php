908b675996cb29d05dbb558ac9cfc89866a3b6db
Michael Torres <michaeljtt@outlook.com>
2023-11-21 13:23:08 -0500

    creando footer.php

--- anuncio.php
+++ anuncio.php
@@ -48,20 +48,6 @@
     <!--!========== FIN MAIN ==========!-->
 
     <!--!========== FOOTER ==========!-->
-    <footer class="footer seccion">
-        <div class="contenedor contenedor_footer">
-            <nav class="navegacion">
-                <a href="nosotros.html">Sobre Nosotros</a>
-                <a href="anuncios.html">Anuncios</a>
-                <a href="blog.html">Blog</a>
-                <a href="contacto.html">Contactanos</a>
-            </nav>
-        </div>
-
-        <p class="copyright">Todos los derechos Reservados 2023 &copy;</p>
-    </footer>
-    <!--!========== FIN FOOTER ==========!-->
-
-    <script src="build/js/bundle.min.js"></script>
-</body>
-</html>
+    <?php
+    include './includes/templades/footer.php';
+?>
