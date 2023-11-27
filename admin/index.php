b06b920b218057448b178c31f4b6f692c72221c2
Michael J. Torres T <117607708+mjtorrest01@users.noreply.github.com>
2023-11-27 18:30:41 -0500

    creando tabla
    creando tabla de registro de propiedades

--- admin/index.php
+++ admin/index.php
@@ -17,6 +17,31 @@
         <?php endif;?>
 
         <a class="boton boton-amarillo" href="/admin/propiedades/crear.php">Nueva Propiedad</a>
+
+            <table class="propiedades">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Titulo</th>
+                        <th>Imagen</th>
+                        <th>Precio</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Casa en la Playa</td>
+                        <td> <img src="/build/img/anuncio1.jpg" class="imagen-tabla"></td>
+                        <td>$120000</td>
+                        <td>
+                            <a href="#" class="boton-rojo-block">Eliminar</a>
+                            <a href="#" class="boton-verde-block">Actualizar</a>
+                        </td>
+                    </tr>
+                </tbody> 
+            </table>
+
     </main>
     <!--!========== FIN MAIN ==========!-->
 
